c02f6689614e6a07d1276cfe77793ac8efa38069
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-02-12 15:56:01 +0200

    the robut telling a Joke

---
+++ src/components/Image.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HaHa from "../images/robot-cute.gif";
+
+const Image = () => {
+  return (
+    <>
+      <img src={HaHa} alt="HA-HA" />
+      <br />
+    </>
+  );
+};
+
+export default Image;
